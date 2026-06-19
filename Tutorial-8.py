8d49a3700f872ad0a02aeb8758402aa06f6eb674
Surydevx <219320689+Surydevx@users.noreply.github.com>
2026-06-19 14:02:00 +0530

    almmost done with this tutorial

--- Tutorial-8.py
+++ Tutorial-8.py
@@ -77,7 +77,7 @@ for line in lines:
  pi_string += line
 print(pi_string)
 print(len(pi_string))
-# i will explain how this code works?
+# i will explain how this code works!
 # everything is predictable and new code is line 75 , we have defined an empty string
 # See line 76 and 77, we have assigned a for loop on the output of the splitlines() method, and we know that this  method returns a list, and thus it means that for loop on line 76, would be looping on this list returned by method "splitlines()", and line 77 is simple, it just concatanates strings, and stores all lines in the variable name "pi_string", well rest of the code is simple.
 # btw in practice this method of string conncatannation is clunky, as strings are immutable to just add a new element to  a string, it recalculates the amount of memory it needs and  asks the os to gie it a new space, and can you imagine running this  loop for an large data?
@@ -101,3 +101,83 @@ path.write_text("I love programming.")
 #Python can only write strings to a text file. If we want to store numerical data in a text file, you’ll have to convert the data to string format first using the str() function.
 
 # To write data more than one line, you would need to store your string in a variable and then pass that variable into write_text()" method.
+
+#Exceptions
+#yk why we  study exceptions and error handling is bcz you don't want you end users to see complex traceback  error messages, it is better that programmme end gracefully with your pre  decided error message than porgramm crashing with a complex traebackw hhich confuses users and more so if you users are malicious hackers, this  traceback would allow them to get more than they should knoiw about your porgrammes stucture and this info could be used maliciously
+# let's start with error and exceptions 
+#Python uses special objects called exceptions to manage errors that arise during a program’s execution.Whenever an error occurs that makes Python unsure of what to do next, it creates an exception object. If you write code that handles the exception, the program will continue running.
+#Exceptions are handled with try- except blocks. A try-except block asks Python to do something, but it also tells Python what to do if an exception is raised.
+#Handling the ZeroDivisionError Exception
+# see this code.
+"""
+print(5/0)
+"""
+# output of the above code
+"""Traceback (most recent call last):
+File "division_calculator.py", line 1, in <module>
+print(5/0)
+~^~
+ZeroDivisionError: division by zero
+"""
+#notie that the error reported in the traceback, ZeroDivisionError, is an exception object in memory.
+# now see the modified code.
+"""
+try:
+print(5/0)
+except ZeroDivisionError:
+print("You can't divide by zero!")
+"""
+# see this, we places our risky code in try, 
+#we place our risky code in try block and we put alternative instructions to run in except block if the code in tyr lock results in a particular kind of exception.
+# though notice that we need to know what kind of exception our porgramme would have and then use try except block to handle that exception, so it's kinda trial and error.
+#notice that if our try blck didn't have raised any exception, then except block of code would have never ran anyways.
+#In this example, the code in the try block produces a ZeroDivisionError, so Python looks for an except block telling it how to respond. Python then runs the code in that block, and the user sees a friendly error message instead of a traceback
+#Using Exceptions to Prevent Crashes
+# Handling errors correctly is especially important when the program has more work to do after the error occurs.
+# see this code.
+"""
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+while True:
+first_number = input("\nFirst number: ")
+second_number = input("Second number: ")
+
+answer = int(first_number) / int(second_number)
+print(answer)
+"""
+# if we input second number as 0, the porgramme would crash literally
+# see code whhich handles this exception,
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+while True:
+ first_number = input("\nFirst number: ")
+ if first_number == 'q':
+  break
+ second_number = input("Second number: ")
+ if second_number == 'q':
+  break
+
+ try:
+  answer = int(first_number) / int(second_number)
+
+ except ValueError:
+  print("please input only numerical values")
+
+ except ZeroDivisionError:
+  print("you are trying to divide by zero, which is actually undefined, and please don't think that it is infinty. ")
+ else:
+  print(answer)
+#This example also includes an else block. Any code that depends on the try block executing successfully goes in the else block. and risky code  itself goes into tty block and if we think a certain type of error can occur we declare an except block telling interpreter what to do in case that crertain error occurs.
+# did u notice that i used two except blocks relative  to two possible exceptions
+# one more  thing is you need to know that yopou won't alwasy know what kind of error and exception you programme into, so during testing phase  you write code like this
+"""
+try:
+    # Some crazy unpredictable code
+    risky_operation()
+    
+except Exception as e:
+    # This catches EVERYTHING, and 'e' is the actual exception object.
+    # It allows you to inspect the exact error without crashing.
+    print(f"I caught an unknown error: {type(e).__name__}")
+"""
+# and then run the code each time you find a new bug or error. consider that this is a sbad practice in actual production env. as ig hides the acctual bugs the code might have.
